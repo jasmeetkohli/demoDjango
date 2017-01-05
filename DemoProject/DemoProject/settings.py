3f62f600547c731c65e596a1b82bb3f2084385f0
Jasmeet Kohli <jasmeetkohlisingh@gmail.com>
2017-01-05 12:11:34 +0530

    db

--- DemoProject/DemoProject/settings.py
+++ DemoProject/DemoProject/settings.py
@@ -79,23 +79,23 @@ MEDIA_URL = '/media/'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'demo',
-#         'USER' : 'db_admin',
-#         'PASSWORD' : 'root',
-#         'HOST' : '35.154.2.160',
-#         'PORT' : '3306',
-
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'demo',
+        'USER' : 'db_admin',
+        'PASSWORD' : 'root',
+        'HOST' : '35.154.2.160',
+        'PORT' : '3306',
+
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
