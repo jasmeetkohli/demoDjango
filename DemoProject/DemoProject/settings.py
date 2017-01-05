44b35572f2165cefb0d262c2e877b981006876e6
Jasmeet Kohli <jasmeetkohlisingh@gmail.com>
2017-01-05 12:09:14 +0530

    no db

--- DemoProject/DemoProject/settings.py
+++ DemoProject/DemoProject/settings.py
@@ -79,15 +79,21 @@ MEDIA_URL = '/media/'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'demo',
+#         'USER' : 'db_admin',
+#         'PASSWORD' : 'root',
+#         'HOST' : '35.154.2.160',
+#         'PORT' : '3306',
+
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'demo',
-        'USER' : 'db_admin',
-        'PASSWORD' : 'root',
-        'HOST' : '35.154.2.160',
-        'PORT' : '3306',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
