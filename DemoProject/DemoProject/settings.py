ebcce8979b49fe5aa61f71bbf6e85a6502237cf8
Jasmeet Kohli <jasmeetkohlisingh@gmail.com>
2017-01-05 12:48:10 +0530

    db1

--- DemoProject/DemoProject/settings.py
+++ DemoProject/DemoProject/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'demo',
         'USER' : 'db_admin',
         'PASSWORD' : 'root',
-        'HOST' : '35.154.2.160',
+        'HOST' : 'localhost',
         'PORT' : '3306',
 
     }
