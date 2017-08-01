3d8edd9966811bd39631f4e548972495ffbd03c2
Jasmeet Singh Kohli <jasmeetkohlisingh@gmail.com>
2017-08-01 19:16:25 +0530

    Update models.py

--- DemoProject/Demo_app/models.py
+++ DemoProject/Demo_app/models.py
@@ -2,8 +2,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
-
 class Customer(models.Model):
 	name = models.CharField(max_length=100)
 	email = models.EmailField(max_length=100)
