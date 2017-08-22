4a81940ea2f38912fb325f2bc5f725cb308f949c
Jasmeet Singh Kohli <jasmeetkohli@Jasmeets-MacBook-Air.local>
2017-08-22 17:13:30 +0530

    play 13.a

--- DemoProject/Demo_app/models.py
+++ DemoProject/Demo_app/models.py
@@ -2,5 +2,5 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Customer(models.Model):
-	name = models.CharField(max_length=100)
-	email = models.EmailField(max_length=100)
+	name = models.CharField(max_length=120)
+	email = models.EmailField(max_length=120)
